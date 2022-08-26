f72cea0571353e46f4412486808d2d4d9304093a
Ishbel95 <ishbel95@hotmail.com>
2022-08-26 22:28:30 +0100

    refactored about.html and about.css

--- src/script.js
+++ src/script.js
@@ -2,16 +2,25 @@ function journey(event) {
   event.preventDefault();
 
   document.querySelector("#about-category").innerHTML = `<div id="journey">
-    <h3 class="text-white pb-4 about-heading">My Journey</h3>
+    <h3 class="text-white about-heading">My Journey</h3>
     <p class="d-block text-white journey-paragraph">
        I've worked as a Special educational needs practioner for many years;
       working in schools, theatre and even educational recruitment. However, with myself and my partner converting a
-      Horse lorry into a tiny house, I knew it was time to realise my dreams and find my passion. And so, I discovered 
-      the wonderous world of code...All I could think was: 'Finally! I've found a realisation of how my brain works!"
-      I begun my journey into Front end development exploring Freecodecamp, Code institute and am 
-      currently continuing my studies at SheCodes. I am an extremely creative, logical and intuitive person; front-end development marries these qualities 
-      together perfectly. Learning to code has changed my life, I have found my passion, my creativity 
-      , my code.
+      Horse lorry into a tiny house, I knew it was time to realise my dreams and find my passion. 
+      <br /> 
+      <br />
+      <strong>So, I discovered 
+      the wonderous world of code...</strong>
+      <br /> 
+      <br />
+      All I could think was: <strong>'Finally! I've found my passion"</strong>
+      I begun my journey into Front end development exploring Freecodecamp and Code institute. I have recently graduated from 
+      <a href="https://www.shecodes.io/graduates/49145-ishbel-fahey-theaker">SheCodes</a> and am currently nearing completion of <a href="https://scrimba.com/dashboard#overview">Scrimba's Frontend career Path.</a> 
+      <br /> 
+      <br />
+      I am an extremely creative, logical and intuitive person; Frontend development marries these qualities 
+      together perfectly. Learning to code has changed my life, I have found my <strong>passion</strong>, my <strong>creativity<strong> 
+      ,  <a href="https://github.com/Ishbel95">my code.</a>
     </p>
   </div>`;
 }
@@ -20,8 +29,7 @@ function hobby(event) {
   event.preventDefault();
 
   document.querySelector("#about-category").innerHTML = ` <div id="hobbies">
-    <h3 class="text-white pb-4 about-heading">My Hobbies</h3>
-    <p class="d-block text-white hobby-heading hobby-paragraph">When i'm not coding I love to...</p>
+    <h3 class="text-white about-heading">My Hobbies</h3>
     <ul class="text-white hobby-list">
       <li>Practice Yoga</li>
       <li>Make clothes</li>
@@ -40,7 +48,7 @@ function stack(event) {
   event.preventDefault();
 
   document.querySelector("#about-category").innerHTML = `<div id="stack">
-    <h3 class="text-white pb-4 about-heading">My Stack</h3>
+    <h3 class="text-white stack-heading">My Stack</h3>
     <div class = "stack-container">
       <i class="fa-brands fa-html5 stack-icon" title="HTML Icon"></i>
       <i class="fa-brands fa-css3 stack-icon" title="CSS Icon"></i>
